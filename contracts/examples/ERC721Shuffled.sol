833dfa63c58e03d09b31b96ce133e6cb0565c2db
Kevin Charm <kevin@kevincharm.com>
2022-09-24 16:38:37 +0200

    ERC721Shuffled: add tests for example

--- contracts/examples/ERC721Shuffled.sol
+++ contracts/examples/ERC721Shuffled.sol
@@ -10,20 +10,33 @@ import {FeistelShuffle} from "../FeistelShuffle.sol";
 contract ERC721Shuffled is ERC721, ERC721Enumerable, Ownable {
     using Strings for uint256;
 
+    /// @notice The first token ID. For most NFT collections, this is 1.
+    uint256 public constant FIRST_TOKEN_ID = 1;
     /// @notice The max supply is used as the modulus parameter in the shuffle algos.
-    uint256 public constant MAX_SUPPLY = 10_000;
+    uint256 public immutable maxSupply;
     /// @notice Random seed that determines the permutation of the shuffle,
     ///     should only be set once.
     bytes32 public randomSeed;
 
-    constructor() ERC721("My NFT Contract", "YEET") {}
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 maxSupply_
+    ) ERC721(name, symbol) {
+        maxSupply = maxSupply_;
+    }
 
     /// @dev Replace this with a call from VRF or some other way to get a secure
     ///     random seed.
     /// @param randomSeed_ random seed
     function setRandomSeed(bytes32 randomSeed_) external onlyOwner {
-        require(randomSeed_ == 0, "already set");
+        require(randomSeed == 0, "already set");
         randomSeed = randomSeed_;
+    }
+
+    /// @notice Example mint function, minting 1-indexed tokenIds (first tokenId = 1)
+    function mint() external {
+        _safeMint(msg.sender, totalSupply() + 1);
     }
 
     /// @notice Return a shuffled tokenURI, calculated just-in-time when this function
@@ -42,16 +55,21 @@ contract ERC721Shuffled is ERC721, ERC721Enumerable, Ownable {
 
         // statelessly map tokenId -> shuffled tokenId,
         // deterministically according to the `randomSeed` and `rounds` parameters
-        // avg ~3.5k gas! wow!
-        uint256 shuffledTokenId = FeistelShuffle.getPermutedIndex(
-            tokenId,
-            MAX_SUPPLY, /** Must stay constant */
-            uint256(randomSeed), /** Must stay constant (once set) */
-            4 /** Must stay constant */
-        );
+        uint256 shuffledTokenId = FIRST_TOKEN_ID +
+            FeistelShuffle.getPermutedIndex(
+                tokenId - FIRST_TOKEN_ID, /** shuffle is 0-indexed, so we add offsets */
+                maxSupply, /** Must stay constant */
+                uint256(randomSeed), /** Must stay constant (once set) */
+                4 /** Must stay constant */
+            );
 
         // use the shuffled tokenId as the metadata index
         return string(abi.encodePacked(_baseURI(), shuffledTokenId.toString()));
+    }
+
+    /// @dev Base URI to concatenate with tokenId
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "ipfs://ipfs/QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR/";
     }
 
     /// @dev Required override
---
+++ test/examples/ERC721Shuffled.spec.ts
@@ -0,0 +1,54 @@
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { BigNumber, BigNumberish } from 'ethers'
+import { randomBytes } from 'crypto'
+import {
+    ERC721Shuffled,
+    ERC721Shuffled__factory,
+    FeistelShuffle__factory,
+} from '../../typechain-types'
+
+describe('FeistelShuffle', () => {
+    let deployer: SignerWithAddress
+    let erc721Shuffled: ERC721Shuffled
+    let seed: BigNumber
+    let maxSupply: number
+    before(async () => {
+        const signers = await ethers.getSigners()
+        deployer = signers[0]
+        const feistelShuffleLibrary = await new FeistelShuffle__factory(deployer).deploy()
+        maxSupply = 1000
+        erc721Shuffled = await new ERC721Shuffled__factory(
+            {
+                'contracts/FeistelShuffle.sol:FeistelShuffle': feistelShuffleLibrary.address,
+            },
+            deployer
+        ).deploy('My NFT contract', 'YEET', maxSupply)
+        seed = BigNumber.from('0x' + randomBytes(32).toString('hex'))
+    })
+
+    function assertSetEquality(left: number[], right: number[]) {
+        const set = new Set<number>()
+        for (const l of left) {
+            set.add(l)
+        }
+        expect(set.size).to.equal(left.length)
+        for (const r of right) {
+            expect(set.delete(r)).to.equal(true, `${r} exists in left`)
+        }
+        expect(set.size).to.equal(0)
+    }
+
+    it('should correctly shuffle tokenIds of an NFT collection', async () => {
+        await erc721Shuffled.setRandomSeed(seed.toHexString())
+        for (let i = 0; i < maxSupply; i++) {
+            await erc721Shuffled.mint()
+        }
+        const tokenUris = new Set<string>()
+        for (let i = 0; i < maxSupply; i++) {
+            tokenUris.add(await erc721Shuffled.tokenURI(i + 1))
+        }
+        expect(tokenUris.size).to.equal(maxSupply)
+    })
+})
